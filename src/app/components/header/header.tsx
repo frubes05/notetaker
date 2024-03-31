7169f36baa46b8d515b6b5a72cdd0723ffe67b02
Filip Rubes <filip.rubes2@gmail.com>
2024-04-01 00:32:21 +0200

    add signin signout button

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import React from "react";
-import { useSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function Header() {
   const { data: sessionData } = useSession();
 
   if (!sessionData?.user) {
-    return null;
+    return  (
+      <div className="flex justify-center items-center">
+        <button className="bg-green-200 p-4 text-white" type="button" onClick={() => signIn()}>
+          Sign In
+        </button>
+      </div>
+    );
   }
 
   return (
@@ -17,7 +23,10 @@ export default function Header() {
             <div className="w-full flex-1 px-5 text-3xl font-bold">
                 <div className="w-full flex justify-between items-center">
                     <p className="text-neutral">Notes for {sessionData.user.name}</p>
-                    <Image className="rounded-full" width={80} height={80} src={sessionData.user.image!} alt={sessionData.user.name!} />
+                    <div className="flex gap-4 items-center">
+                      <button className="bg-red-200 p-4 text-white" type="button" onClick={() => signOut()}>Sign out</button>
+                      <Image className="rounded-full" width={80} height={80} src={sessionData.user.image!} alt={sessionData.user.name!} />
+                    </div>
                 </div>
             </div>
         </div>
